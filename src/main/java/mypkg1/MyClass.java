b114f57d4a786893af5a95f6fa3b41c10a732c4f
Ujjwal Kumar Gupta <ujjwalgupta1.ujg@gmail.com>
2023-02-01 22:42:34 +0530

    first commit

---
+++ src/main/java/mypkg1/MyClass.java
@@ -0,0 +1,16 @@
+package mypkg1;
+
+public class MyClass {
+	public void show()
+	{
+		System.out.println("hello jenkins ");
+	}
+	
+
+	public static void main(String[] args) {
+		
+		MyClass m1= new MyClass();
+		m1.show();
+	}
+
+}
