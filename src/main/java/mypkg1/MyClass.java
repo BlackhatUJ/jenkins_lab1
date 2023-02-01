98a50eb5f730d1ec767ec4895e94ce8a58b1a14e
Ujjwal Kumar Gupta <ujjwalgupta1.ujg@gmail.com>
2023-02-02 00:20:57 +0530

    second commit

--- src/main/java/mypkg1/MyClass.java
+++ src/main/java/mypkg1/MyClass.java
@@ -5,12 +5,18 @@ public class MyClass {
 	{
 		System.out.println("hello jenkins ");
 	}
+	public void add()
+	{
+		int a=5,b=10;
+		System.out.println(a+b);
+	}
 	
 
 	public static void main(String[] args) {
 		
 		MyClass m1= new MyClass();
 		m1.show();
+		m1.add();
 	}
 
 }
